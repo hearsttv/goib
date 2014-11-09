72c1f1106d601783e2411b4c12789bc4fc3fe51c
Nathan Smith <nate@branchout.com>
2014-11-09 12:12:26 -0800

    regenerated mockery mocks

--- mocks/API.go
+++ mocks/API.go
@@ -9,32 +9,32 @@ type API struct {
 	mock.Mock
 }
 
-func (m *API) Entry(entrytype string, params url.Values) (goib.Collection, error) {
-	ret := m.Called(entrytype, params)
+func (m *API) Entry(channel string, entrytype string, params url.Values) (goib.Collection, error) {
+	ret := m.Called(channel, entrytype, params)
 
 	r0 := ret.Get(0).(goib.Collection)
 	r1 := ret.Error(1)
 
 	return r0, r1
 }
-func (m *API) Article(contentID int, params url.Values) (goib.Article, error) {
-	ret := m.Called(contentID, params)
+func (m *API) Article(channel string, contentID int, params url.Values) (goib.Article, error) {
+	ret := m.Called(channel, contentID, params)
 
 	r0 := ret.Get(0).(goib.Article)
 	r1 := ret.Error(1)
 
 	return r0, r1
 }
-func (m *API) Video(contentID int, params url.Values) (goib.Video, error) {
-	ret := m.Called(contentID, params)
+func (m *API) Video(channel string, contentID int, params url.Values) (goib.Video, error) {
+	ret := m.Called(channel, contentID, params)
 
 	r0 := ret.Get(0).(goib.Video)
 	r1 := ret.Error(1)
 
 	return r0, r1
 }
-func (m *API) Search(query string, params url.Values) (goib.SearchResult, error) {
-	ret := m.Called(query, params)
+func (m *API) Search(channel string, query string, params url.Values) (goib.SearchResult, error) {
+	ret := m.Called(channel, query, params)
 
 	r0 := ret.Get(0).(goib.SearchResult)
 	r1 := ret.Error(1)
