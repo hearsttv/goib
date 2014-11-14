db59afd577939c3b2057b687c79f82ffb7025d41
Nathan Smith <nate@branchout.com>
2014-11-13 16:09:28 -0800

    add a way to iterate over the contents of nested collections, extracting the media while also exposing the parent collection that each media belongs to i'm using a channel to publish each iteration and a recursive function to actually do the iteration usage: for _, mediaNode := range ib.Iterator(rootNode) { /* do work */ }

--- util.go
+++ util.go
@@ -47,3 +47,49 @@ func extractMediaRecursive(input interface{}, media *[]interface{}) {
 
 	return
 }
+
+type MediaNode struct {
+	Media            interface{}
+	ParentCollection interface{}
+}
+
+func Iterator(root interface{}) chan *MediaNode {
+	ch := make(chan *MediaNode)
+	go iterateMedia(root, ch)
+	return ch
+}
+
+func iterateMedia(root interface{}, ch chan *MediaNode) {
+	iterateMediaRecursive(root, nil, ch)
+	close(ch)
+}
+
+func iterateMediaRecursive(node interface{}, parent interface{}, ch chan *MediaNode) {
+	if node == nil {
+		return
+	}
+
+	switch t := node.(type) {
+	case Article:
+		ch <- &MediaNode{t, parent}
+	case Video:
+		ch <- &MediaNode{t, parent}
+	case Image:
+		ch <- &MediaNode{t, parent}
+	case Gallery:
+		ch <- &MediaNode{t, parent}
+	case Collection:
+		if t.Items == nil || len(t.Items) == 0 {
+			return
+		}
+		for _, item := range t.Items {
+			iterateMediaRecursive(item, t, ch)
+		}
+
+	default:
+		log.Warn("unexpected type found during iteration: %t", t)
+		break
+	}
+
+	return
+}
